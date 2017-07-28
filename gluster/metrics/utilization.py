cf4b4442ddd18be39513b14d805afd985013d7d7
Aravinda VK <mail@aravindavk.in>
2017-07-28 12:32:21 +0530

    metrics: Added code documentation
    for local_utilization and local_diskstats
    
    Signed-off-by: Aravinda VK <mail@aravindavk.in>

--- gluster/metrics/utilization.py
+++ gluster/metrics/utilization.py
@@ -4,6 +4,26 @@ from utils import get_local_bricks
 
 
 def local_utilization(volname=None):
+    """
+    Collect Utilization details of local bricks
+
+    :param volname: Volume Name
+    :returns: List of utilization information
+        {               
+            "volume": VOLUME_NAME,
+            "brick_index": BRICK_INDEX_IN_VOL_INFO,
+            "node_id": NODE_ID,
+            "brick": BRICK_NAME,
+            "block_size": ST_F_FRSIZE,
+            "blocks_total": ST_F_BLOCKS,
+            "blocks_free": ST_F_BFREE,
+            "blocks_avail": ST_F_BAVAIL,
+            "inodes_total": ST_F_FILES,
+            "inodes_free": ST_F_FFREE,
+            "inodes_avail": ST_F_FAVAIL
+        }
+    """
+
     local_bricks = get_local_bricks(volname)
 
     for brick in local_bricks:
