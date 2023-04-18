0adadf6b67b3830ab44c698aea2bdbf1762bf03d
Aravinda Vishwanathapura <aravinda@kadalu.tech>
2023-04-18 21:29:39 +0530

    Master/slave to Primary/secondary changes
    Signed-off-by: Aravinda Vishwanathapura <aravinda@kadalu.tech>

--- glustercli/cli/georep.py
+++ glustercli/cli/georep.py
@@ -23,26 +23,29 @@ def gsec_create(ssh_key_prefix=True):
 
 
 # noqa # pylint: disable=too-many-arguments
-def create(volname, slave_host, slave_vol, slave_user="root",
+def create(primary_volume, secondary_host, secondary_volume,
+           secondary_user="root",
            push_pem=True, no_verify=False, force=False, ssh_port=22):
     """
     Create Geo-replication Session
 
-    :param volname: Master Volume Name
-    :param slave_host: Slave Hostname or IP
-    :param slave_vol: Slave Volume
-    :param slave_user: Slave User, default is "root"
-    :param push_pem: True|False Push SSH keys to Slave
-    :param no_verify: True|False Skip the Slave Verification
+    :param primary_volume: Primary Volume Name
+    :param secondary_host: Secondary Hostname or IP
+    :param secondary_volume: Secondary Volume
+    :param secondary_user: Secondary User, default is "root"
+    :param push_pem: True|False Push SSH keys to Secondary
+    :param no_verify: True|False Skip the Secondary Verification
      process before create
     :param force: True|False Force Create Session
     :param ssh_port: SSH Port, Default is 22
     :returns: Output of Create command, raises
      GlusterCmdException((rc, out, err)) on error
     """
-    cmd = [volname,
-           "{0}@{1}::{2}".format(slave_user, slave_host, slave_vol),
-           "create"]
+    cmd = [
+        primary_volume,
+        f"{secondary_user}@{secondary_host}::{secondary_volume}",
+        "create"
+    ]
 
     if ssh_port != 22:
         cmd += ["ssh-port", "{0}".format(ssh_port)]
@@ -59,20 +62,21 @@ def create(volname, slave_host, slave_vol, slave_user="root",
     return georep_execute(cmd)
 
 
-def start(volname, slave_host, slave_vol, slave_user="root", force=False):
+def start(primary_volume, secondary_host, secondary_volume,
+          secondary_user="root", force=False):
     """
     Start Geo-replication Session
 
-    :param volname: Master Volume Name
-    :param slave_host: Slave Hostname or IP
-    :param slave_vol: Slave Volume
-    :param slave_user: Slave User, default is "root"
+    :param primary_volume: Primary Volume Name
+    :param secondary_host: Secondary Hostname or IP
+    :param secondary_volume: Secondary Volume
+    :param secondary_user: Secondary User, default is "root"
     :param force: True|False Force Start the Session
     :returns: Output of Start command, raises
      GlusterCmdException((rc, out, err)) on error
     """
-    cmd = [volname,
-           "{0}@{1}::{2}".format(slave_user, slave_host, slave_vol),
+    cmd = [primary_volume,
+           f"{secondary_user}@{secondary_host}::{secondary_volume}",
            "start"]
 
     if force:
@@ -81,20 +85,21 @@ def start(volname, slave_host, slave_vol, slave_user="root", force=False):
     return georep_execute(cmd)
 
 
-def stop(volname, slave_host, slave_vol, slave_user="root", force=False):
+def stop(primary_volume, secondary_host, secondary_volume,
+         secondary_user="root", force=False):
     """
     Stop Geo-replication Session
 
-    :param volname: Master Volume Name
-    :param slave_host: Slave Hostname or IP
-    :param slave_vol: Slave Volume
-    :param slave_user: Slave User, default is "root"
-    :param force: True|False Force Stop the Session
+    :param primary_volume: Primary Volume Name
+    :param secondary_host: Secondary Hostname or IP
+    :param secondary_volume: Secondary Volume
+    :param secondary_user: Secondary User, default is "root"
+    :param force: True|False Force Start the Session
     :returns: Output of Stop command, raises
      GlusterCmdException((rc, out, err)) on error
     """
-    cmd = [volname,
-           "{0}@{1}::{2}".format(slave_user, slave_host, slave_vol),
+    cmd = [primary_volume,
+           f"{secondary_user}@{secondary_host}::{secondary_volume}",
            "stop"]
 
     if force:
@@ -103,37 +108,40 @@ def stop(volname, slave_host, slave_vol, slave_user="root", force=False):
     return georep_execute(cmd)
 
 
-def restart(volname, slave_host, slave_vol, slave_user="root", force=False):
+def restart(primary_volume, secondary_host, secondary_volume,
+            secondary_user="root", force=False):
     """
     Restart Geo-replication Session
 
-    :param volname: Master Volume Name
-    :param slave_host: Slave Hostname or IP
-    :param slave_vol: Slave Volume
-    :param slave_user: Slave User, default is "root"
+    :param primary_volume: Primary Volume Name
+    :param secondary_host: Secondary Hostname or IP
+    :param secondary_volume: Secondary Volume
+    :param secondary_user: Secondary User, default is "root"
     :param force: True|False Force Start the Session
     :returns: Output of Start command, raises
      GlusterCmdException((rc, out, err)) on error
     """
-    stop(volname, slave_host, slave_vol, slave_user, force=True)
-    return start(volname, slave_host, slave_vol, slave_user, force)
+    stop(primary_volume, secondary_host, secondary_volume,
+         secondary_user, force=True)
+    return start(primary_volume, secondary_host, secondary_volume,
+                 secondary_user, force)
 
 
-def delete(volname, slave_host, slave_vol, slave_user="root",
-           reset_sync_time=None):
+def delete(primary_volume, secondary_host, secondary_volume,
+           secondary_user="root", reset_sync_time=None):
     """
     Delete Geo-replication Session
 
-    :param volname: Master Volume Name
-    :param slave_host: Slave Hostname or IP
-    :param slave_vol: Slave Volume
-    :param slave_user: Slave User, default is "root"
+    :param primary_volume: Primary Volume Name
+    :param secondary_host: Secondary Hostname or IP
+    :param secondary_volume: Secondary Volume
+    :param secondary_user: Secondary User, default is "root"
     :param reset_sync_time: True|False Reset Sync time on delete
     :returns: Output of Start command, raises
      GlusterCmdException((rc, out, err)) on error
     """
-    cmd = [volname,
-           "{0}@{1}::{2}".format(slave_user, slave_host, slave_vol),
+    cmd = [primary_volume,
+           f"{secondary_user}@{secondary_host}::{secondary_volume}"
            "delete"]
 
     if reset_sync_time is not None:
@@ -142,20 +150,21 @@ def delete(volname, slave_host, slave_vol, slave_user="root",
     return georep_execute(cmd)
 
 
-def pause(volname, slave_host, slave_vol, slave_user="root", force=False):
+def pause(primary_volume, secondary_host, secondary_volume,
+          secondary_user="root", force=False):
     """
     Pause Geo-replication Session
 
-    :param volname: Master Volume Name
-    :param slave_host: Slave Hostname or IP
-    :param slave_vol: Slave Volume
-    :param slave_user: Slave User, default is "root"
+    :param primary_volume: Primary Volume Name
+    :param secondary_host: Secondary Hostname or IP
+    :param secondary_volume: Secondary Volume
+    :param secondary_user: Secondary User, default is "root"
     :param force: True|False Force Pause Session
     :returns: Output of Pause command, raises
      GlusterCmdException((rc, out, err)) on error
     """
-    cmd = [volname,
-           "{0}@{1}::{2}".format(slave_user, slave_host, slave_vol),
+    cmd = [primary_volume,
+           f"{secondary_user}@{secondary_host}::{secondary_volume}"
            "pause"]
 
     if force:
@@ -164,20 +173,21 @@ def pause(volname, slave_host, slave_vol, slave_user="root", force=False):
     return georep_execute(cmd)
 
 
-def resume(volname, slave_host, slave_vol, slave_user="root", force=False):
+def resume(primary_volume, secondary_host, secondary_volume,
+           secondary_user="root", force=False):
     """
     Resume Geo-replication Session
 
-    :param volname: Master Volume Name
-    :param slave_host: Slave Hostname or IP
-    :param slave_vol: Slave Volume
-    :param slave_user: Slave User, default is "root"
+    :param primary_volume: Primary Volume Name
+    :param secondary_host: Secondary Hostname or IP
+    :param secondary_volume: Secondary Volume
+    :param secondary_user: Secondary User, default is "root"
     :param force: True|False Force Resume Session
     :returns: Output of Resume command, raises
      GlusterCmdException((rc, out, err)) on error
     """
-    cmd = [volname,
-           "{0}@{1}::{2}".format(slave_user, slave_host, slave_vol),
+    cmd = [primary_volume,
+           f"{secondary_user}@{secondary_host}::{secondary_volume}"
            "resume"]
 
     if force:
@@ -186,59 +196,61 @@ def resume(volname, slave_host, slave_vol, slave_user="root", force=False):
     return georep_execute(cmd)
 
 
-def config_set(volname, slave_host, slave_vol, key, value,
-               slave_user="root"):
+def config_set(primary_volume, secondary_host, secondary_volume,
+               key, value,
+               secondary_user="root"):
     """
     Set Config of a Geo-replication Session
 
-    :param volname: Master Volume Name
-    :param slave_host: Slave Hostname or IP
-    :param slave_vol: Slave Volume
-    :param slave_user: Slave User, default is "root"
+    :param primary_volume: Primary Volume Name
+    :param secondary_host: Secondary Hostname or IP
+    :param secondary_volume: Secondary Volume
+    :param secondary_user: Secondary User, default is "root"
     :param key: Config Key
     :param value: Config Value
     :returns: Output of Config set command, raises
      GlusterCmdException((rc, out, err)) on error
     """
-    cmd = [volname,
-           "{0}@{1}::{2}".format(slave_user, slave_host, slave_vol),
+    cmd = [primary_volume,
+           f"{secondary_user}@{secondary_host}::{secondary_volume}"
            "config", key, value]
     return georep_execute(cmd)
 
 
-def config_reset(volname, slave_host, slave_vol, key, slave_user="root"):
+def config_reset(primary_volume, secondary_host, secondary_volume,
+                 key, secondary_user="root"):
     """
     Reset configuration of Geo-replication Session
 
-    :param volname: Master Volume Name
-    :param slave_host: Slave Hostname or IP
-    :param slave_vol: Slave Volume
-    :param slave_user: Slave User, default is "root"
+    :param primary_volume: Primary Volume Name
+    :param secondary_host: Secondary Hostname or IP
+    :param secondary_volume: Secondary Volume
+    :param secondary_user: Secondary User, default is "root"
     :param key: Config Key
     :returns: Output of Config reset command, raises
      GlusterCmdException((rc, out, err)) on error
     """
-    cmd = [volname,
-           "{0}@{1}::{2}".format(slave_user, slave_host, slave_vol),
+    cmd = [primary_volume,
+           f"{secondary_user}@{secondary_host}::{secondary_volume}"
            "config", "!{0}".format(key)]
     return georep_execute(cmd)
 
 
-def config_get(volname, slave_host, slave_vol, key=None,
-               slave_user="root"):
+def config_get(primary_volume, secondary_host, secondary_volume, key=None,
+               secondary_user="root"):
     """
     Get Configuration of Geo-replication Session
 
-    :param volname: Master Volume Name
-    :param slave_host: Slave Hostname or IP
-    :param slave_vol: Slave Volume
-    :param slave_user: Slave User, default is "root"
+    :param primary_volume: Primary Volume Name
+    :param secondary_host: Secondary Hostname or IP
+    :param secondary_volume: Secondary Volume
+    :param secondary_user: Secondary User, default is "root"
     :param key: Config Key
     :returns: Geo-rep session Config Values, raises
      GlusterCmdException((rc, out, err)) on error
     """
-    cmd = [volname,
-           "{0}@{1}::{2}".format(slave_user, slave_host, slave_vol),
+    cmd = [primary_volume,
+           f"{secondary_user}@{secondary_host}::{secondary_volume}"
            "config"]
 
     if key is not None:
@@ -247,26 +259,29 @@ def config_get(volname, slave_host, slave_vol, key=None,
     return parse_georep_config(georep_execute_xml(cmd))
 
 
-def status(volname=None, slave_host=None, slave_vol=None,
-           slave_user="root"):
+def status(primary_volume=None, secondary_host=None,
+           secondary_volume=None,
+           secondary_user="root"):
     """
     Status of Geo-replication Session
 
-    :param volname: Master Volume Name
-    :param slave_host: Slave Hostname or IP
-    :param slave_vol: Slave Volume
-    :param slave_user: Slave User, default is "root"
+    :param primary_volume: Primary Volume Name
+    :param secondary_host: Secondary Hostname or IP
+    :param secondary_volume: Secondary Volume
+    :param secondary_user: Secondary User, default is "root"
     :returns: Geo-replication Status, raises
      GlusterCmdException((rc, out, err)) on error
     """
     cmd = []
 
-    if volname is not None:
-        cmd += [volname]
+    if primary_volume is not None:
+        cmd += [primary_volume]
 
-    if volname is not None and slave_host is not None and \
-       slave_vol is not None:
-        cmd += ["{0}@{1}::{2}".format(slave_user, slave_host, slave_vol)]
+    if primary_volume is not None and secondary_host is not None and \
+       secondary_volume is not None:
+        cmd += [
+            f"{secondary_user}@{secondary_host}::{secondary_volume}"
+        ]
 
     cmd += ["status"]
 
